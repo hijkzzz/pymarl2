aca34efc8ec3cf4e758b29ffd2171d22b77f5957
hijkzzz <janhu9527@gmail.com>
2021-02-20 23:01:45 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,6 @@
 # conda create -n pymarl python=3.7 -y
 # conda activate pymarl
 
-conda install pytorch torchvision cudatoolkit=10.2 -c pytorch -y
+conda install pytorch torchvision cudatoolkit=11.0 -c pytorch -y
 pip install sacred numpy scipy gym==0.10.8 matplotlib seaborn pyyaml pygame pytest probscale imageio snakeviz tensorboard-logger
 pip install git+https://github.com/oxwhirl/smac.git
