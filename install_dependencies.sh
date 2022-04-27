6b64139aebad208f198868af9f9d52af900bc695
benellis3 <benellis@robots.ox.ac.uk>
2022-04-27 14:19:21 +0100

    Add pip update

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,6 +4,7 @@
 # conda create -n pymarl python=3.7 -y
 # conda activate pymarl
 
+pip install --upgrade pip
 pip install --ignore-installed six
 pip install sacred numpy scipy gym==0.10.8 matplotlib seaborn pyyaml pygame pytest probscale imageio snakeviz tensorboard-logger wandb
-pip install git+https://github.com/oxwhirl/smac.git
+pip install git+https://github.com/benellis3/smac.git@smac-v2
