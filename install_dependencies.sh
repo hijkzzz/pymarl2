4969c8475bbc22e68eb1ecfff0cf3d85d9aad55f
hijkzzz <janhu9527@gmail.com>
2022-10-21 08:51:26 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,7 +4,7 @@
 # conda create -n pymarl python=3.8 -y
 # conda activate pymarl
 
-conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch-lts -c nvidia -y
+conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch -y
 pip install protobuf==3.20.2 sacred==0.8.2 numpy scipy gym==0.11 matplotlib seaborn \
     pyyaml==5.3.1 pygame pytest probscale imageio snakeviz tensorboard-logger
 pip install git+https://github.com/oxwhirl/smac.git
