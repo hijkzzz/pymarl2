a44dbf221bc08b617b05b3d31559faea96925bf9
jokeroller <jokeroller@163.com>
2022-12-01 15:07:19 +0800

    add NDQ algorithm and a requirement file.

--- run_ndq.sh
+++ run_ndq.sh
@@ -1,6 +1,5 @@
-python src/main.py --config=ndq --env-config=sc2x with env_args.map_name=2s3z
-# \
-#env_args.sight_range=2 \
-#env_args.shoot_range=2 \
-#env_args.obs_all_health=False \
-#env_args.obs_enemy_health=False
+python src/main.py --config=ndq --env-config=sc2x with env_args.map_name=bane_vs_hM \
+env_args.sight_range=2 \
+env_args.shoot_range=2 \
+env_args.obs_all_health=False \
+env_args.obs_enemy_health=False
