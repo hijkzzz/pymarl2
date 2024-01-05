1254df6f0cb9d1fd2b7c60566a5e23eff8f0528e
hijkzzz <janhu9527@gmail.com>
2024-01-05 15:56:46 +0800

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,6 +5,6 @@
 # conda activate pymarl
 
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch -y
-pip install protobuf==3.19.5 sacred==0.8.2 numpy scipy gym==0.11 matplotlib seaborn \
+pip install protobuf==3.19.5 sacred==0.7.5 numpy scipy gym==0.11 matplotlib seaborn \
     pyyaml==5.3.1 pygame pytest probscale imageio snakeviz tensorboard-logger
 pip install git+https://github.com/oxwhirl/smac.git@26f4c4e4d1ebeaf42ecc2d0af32fac0774ccc678
